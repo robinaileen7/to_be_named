3389ab670cb041d026457658ef64b92c5e85810d
robinaileen7 <robinaileen7@g.ucla.edu>
2025-02-09 18:58:08 -0800

    Added Moving Directions for Real and Img Parts of the Particles

--- particles_moving.py
+++ particles_moving.py
@@ -53,10 +53,14 @@ class record_states:
         def init():
             ax.set_xlim(-1, 10)
             ax.set_ylim(-2.2, 1.5)
+            plt.xlabel('Real')
+            plt.ylabel('Img')
 
         def init_norm():
-            ax.set_xlim(0.06, 0.11)
-            ax.set_ylim(-0.08, 0.08)
+            ax.set_xlim(0, 0.15)
+            ax.set_ylim(-0.15, 0.15)
+            plt.xlabel('Real')
+            plt.ylabel('Img')
 
         def update(i):
             use_init = True
@@ -71,7 +75,47 @@ class record_states:
         animation = FuncAnimation(fig, update, frames=param[3] + 1, interval=200, repeat=False)
         plt.show()
 
-if __name__ == "__main__":
-    param = [100, 0.1, math.pi/4, 100]
+    @staticmethod
+    def draw_dir(norm_adj):
+        data = record_states(param).save(norm_adj)
+        data_1 = data[:-1]
+        data_2 = data[1:]
+        fig, ax = plt.subplots()
 
-    record_states(param).draw_states(norm_adj=True)
+        def init():
+            plt.xlabel('Position')
+            ax.set_ylim(-3, 8)
+            plt.legend()
+
+        def init_norm():
+            plt.xlabel('Position')
+            ax.set_ylim(-0.2, 0.2)
+            plt.legend()
+
+        def update(i):
+            use_init = True
+            ax.clear()
+            x_grid = np.arange(0, param[0], 1)
+            ax.scatter(x_grid, data[i][0], s=2, color='red', label='Real')
+            ax.scatter(x_grid, data[i][1], s=2, color='blue', label='Img')
+            for j in range(len(x_grid)):
+                ax.arrow(x_grid[j], data_1[i][0][j], 0, data_2[i][0][j]-data_1[i][0][j], head_width=0.5,
+                         head_length=0.01, fc='red', ec='red')
+                ax.arrow(x_grid[j], data_1[i][1][j], 0, data_2[i][1][j] - data_1[i][1][j], head_width=0.5,
+                         head_length=0.01, fc='blue', ec='blue')
+            plt.grid()
+            if use_init:
+                if norm_adj:
+                    init_norm()
+                else:
+                    init()
+
+        animation = FuncAnimation(fig, update, frames=param[3], interval=1000, repeat=False)
+        plt.show()
+
+if __name__ == "__main__":
+    param = [50, 0.1, math.pi / 4, 100]
+
+    obj = record_states(param)
+    obj.draw_states(norm_adj=True)
+    obj.draw_dir(norm_adj=True)
